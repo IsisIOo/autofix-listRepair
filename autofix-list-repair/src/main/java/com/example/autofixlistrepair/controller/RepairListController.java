4f2f47a1d5f727cbd3248fdf11f4e49a216d69e1
Isidora <isidora.oyanedel@usach.cl>
2024-06-06 21:01:47 -0400

    efefef

--- autofix-list-repair/src/main/java/com/example/autofixlistrepair/controller/RepairListController.java
+++ autofix-list-repair/src/main/java/com/example/autofixlistrepair/controller/RepairListController.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api/repair-list")
-@CrossOrigin("*")
+
 public class RepairListController {
     @Autowired
     RepairListService repairListService;
